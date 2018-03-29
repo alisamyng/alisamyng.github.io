3679b5f78b688621f5f29a6926b515d0b93d1c45
Alisa Ng <alisang37@gmail.com>
2018-03-29 15:07:50 +0100

    set up

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'portfolio', to: 'pages#portfolio', as: :portfolio
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
